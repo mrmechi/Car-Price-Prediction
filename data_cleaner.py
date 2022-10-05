26708b349f19c7a68beb2b5bd01c5685dcbb43bb
Ashish Raj Purohit <ashishpurohit121@gmail.com>
2022-10-06 00:16:27 +0530

    Car Price Predictor

---
+++ data_cleaner.py
@@ -0,0 +1,78 @@
+
+
+class Cleaner:
+
+    def num_year(self, data,column):
+            self.data=data
+            self.column=column
+            
+            self.year_in_num=self.data[self.data[self.column].str.isnumeric()]
+            return self.year_in_num
+
+
+    def int_year(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data[self.column]=self.data[self.column].astype(int)
+        return self.data
+
+    
+
+    def drop_non_int_in_price(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data=self.data[self.data[self.column]!='Ask For Price']
+        return self.data
+
+
+    def conv_str_to_int_in_price(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data[self.column]=self.data[self.column].str.replace(',','').astype(int)
+        return self.data
+        
+
+    def formatting_km_driven(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data[self.column]=self.data[self.column].str.split().str.get(0).str.replace(',','')
+        return self.data
+
+
+    def conv_str_to_int_in_km_driven(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data=self.data[self.data[self.column].str.isnumeric()]
+        return self.data
+
+    def removing_na_in_fuel_type(self,data,column):
+        self.data=data
+        self.column=column
+        self.data=self.data[~self.data[self.column].isna()]
+        return self.data
+
+    def fixing_name(self,data,column):
+        self.data=data
+        self.column=column
+
+        self.data[self.column]=self.data[self.column].str.split().str.slice(start=0,stop=3).str.join(' ')
+        return self.data
+
+
+    def index(self,data):
+        self.data=data
+
+        self.index=self.data.reset_index(drop=True)
+        return self.index
+
+
+    def csv(self,data):
+        self.data=data
+
+        self.cleaned_data=self.data.to_csv('Cleaned_car_data.csv')
+        return self.cleaned_data
---
+++ data_loader.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+class Data_Getter:
+    """ This class shall  be used for obtaining the data from the source for training. """
+
+    
+    def __init__(self):
+        self.training_file='cardata.csv'
+
+
+    def get_data(self):
+        try:
+            data=pd.read_csv(self.training_file)
+            return data 
+        except:
+            print("Unable to load the dataset")
+
+# a=Data_Getter()
+# print(a.get_data(),'aaaaaaaaaaaaaaaaaaa')
